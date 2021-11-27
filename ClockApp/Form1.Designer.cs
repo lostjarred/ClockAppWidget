446ad41dc7f6d90a101bd95960be07b41bd03c89
lostjarred <lostone1993@hotmail.com>
2021-11-27 14:54:24 +1100

    Add background image

--- ClockApp/Form1.Designer.cs
+++ ClockApp/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.ConsoleTextBox = new System.Windows.Forms.RichTextBox();
             this.DecTime = new System.Windows.Forms.TextBox();
             this.Bintime = new System.Windows.Forms.TextBox();
@@ -154,6 +155,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.HexDate);
             this.Controls.Add(this.OctDate);
@@ -168,6 +172,7 @@
             this.Controls.Add(this.Bintime);
             this.Controls.Add(this.DecTime);
             this.Controls.Add(this.ConsoleTextBox);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
