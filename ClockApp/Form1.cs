d428cfca5b5bf1166699e976f9b189bea1f21d4a
lostjarred <lostone1993@hotmail.com>
2021-12-04 09:50:19 +1100

    Clean up code a bit

--- ClockApp/Form1.cs
+++ ClockApp/Form1.cs
@@ -26,20 +26,28 @@ namespace ClockApp
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            var datetimenow = DateTime.Now.ToString();
+            updateDec();
+            updateBin();
+            updateOct();
+            updateHex();
+            ConsoleTextBox.AppendText(Environment.NewLine + "> Updating clock");
+        }
+
+        public void updateDec() {
             DecTime.Text = DateTimeUtils.getHour().ToString() + ":" + DateTimeUtils.getMinute().ToString() + ":" + DateTimeUtils.getSecond().ToString();
             DecDate.Text = DateTimeUtils.getDay().ToString() + "/" + DateTimeUtils.getMonth().ToString() + "/" + DateTimeUtils.getYear().ToString();
-            
-            Bintime.Text = DateTimeUtils.getHourbin() + ":" + DateTimeUtils.getMinutebin()+ ":" + DateTimeUtils.getSecondbin();
-            BinDate.Text = DateTimeUtils.getDaybin() + "/" + DateTimeUtils.getMonthbin()+ "/" + DateTimeUtils.getYearbin();
-
+        }
+        public void updateBin() {
+            Bintime.Text = DateTimeUtils.getHourbin() + ":" + DateTimeUtils.getMinutebin() + ":" + DateTimeUtils.getSecondbin();
+            BinDate.Text = DateTimeUtils.getDaybin() + "/" + DateTimeUtils.getMonthbin() + "/" + DateTimeUtils.getYearbin();
+        }
+        public void updateOct() {
             OctTime.Text = DateTimeUtils.getHouroct() + ":" + DateTimeUtils.getMinuteoct() + ":" + DateTimeUtils.getSecondoct();
             OctDate.Text = DateTimeUtils.getDayoct() + "/" + DateTimeUtils.getMonthoct() + "/" + DateTimeUtils.getYearoct();
-
+        }
+        public void updateHex() {
             HexTime.Text = DateTimeUtils.getHourhex() + ":" + DateTimeUtils.getMinutehex() + ":" + DateTimeUtils.getSecondhex();
             HexDate.Text = DateTimeUtils.getDayhex() + "/" + DateTimeUtils.getMonthhex() + "/" + DateTimeUtils.getYearhex();
-
-            ConsoleTextBox.AppendText(Environment.NewLine + "> Updating clock");
         }
     }
 }
